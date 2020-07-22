874f7e24b1cb68d1b452605705b4a9b0ef8b917c
DarianRushworth <darianerrolrushworth@gmail.com>
2020-07-22 22:56:53 +0200

    Setup Router

--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from "react-redux"
 import store from "./store"
+import { BrowserRouter as Router } from "react-router-dom"
+
 
 ReactDOM.render(
   <React.StrictMode>
+    <Router>
     <Provider store={store}>
     <App />
     </Provider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
