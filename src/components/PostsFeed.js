d9df2e0f3b4b0ff1d98cfea7e555e651b2203e04
DarianRushworth <darianerrolrushworth@gmail.com>
2020-07-23 09:34:42 +0200

    Displaying Loading whilst waiting

--- src/components/PostsFeed.js
+++ src/components/PostsFeed.js
@@ -7,19 +7,19 @@ const postsUrl = `https://codaisseur-coders-network.herokuapp.com/posts`;
 
 export default function PostsFeed() {
   const [data, setData] = useState({
-    loading: false,
+    loading: true,
     posts: []
   });
 
   async function fetchNext5Posts() {
-    setData({ ...data, loading: false });
+    setData({ ...data, loading: true });
     const response = await axios.get(`${postsUrl}?offset=${data.posts.length}&limit=5`)
     // console.log("response test", response.data.rows)
     // console.log("what is my data so far", data)
     const morePosts = response.data.rows
 
     setData({
-      loading: true,
+      loading: false,
       posts: [...data.posts, ...morePosts]
     });
   }
@@ -28,37 +28,38 @@ export default function PostsFeed() {
     fetchNext5Posts();
   }, []);
 
-//   console.log("outside the function test", data)
+  console.log("outside the function test", data)
   const displayPosts = () => {
-        return data.posts.map(post => {
-            return (
-                    <div key={post.id}>
-                        <h4>{post.title}</h4>
-                        <p className="meta">
-                        {moment(post.createdAt).format("DD-MM-YYYY")} &bull;{" "}
-                        </p>
-                        <span className="tags">
-                        {post.tags.map(tag => {
-                            return (
-                                <React.Fragment key={tag.id}>
-                                    <span className="Tag">{tag.tag}</span>
-                                </React.Fragment>
-                            )
-                        })}
-                        </span>
-                    </div>
-            )
-        })
-    } 
-  
+    if(!data.loading){
+      return data.posts.map(post => {
+      return (
+        <div key={post.id}>
+          <h4>{post.title}</h4>
+          <p className="meta">
+            {moment(post.createdAt).format("DD-MM-YYYY")} &bull;{" "}
+          </p>
+          <span className="tags">
+            {post.tags.map(tag => {
+              return (
+                <React.Fragment key={tag.id}>
+                  <span className="Tag">{tag.tag}</span>
+                </React.Fragment>
+              )
+            })}
+          </span>
+        </div>
+      )
+    })
+    } else {
+      return <h1>Loading...</h1>
+    }
+  }
+
 
   return (
     <div className="PostsFeed">
       <h2>Recent posts</h2>
-        {displayPosts()}
-
-      {/* TODO: show a loading indicator when the posts are loading,
-        or else a button to load more posts if not */}
+      {displayPosts()}
     </div>
   );
 }
